ae846e7476c4f7a222e8254496f8e4a22f00d30a
RexShack <rexshackuk@gmail.com>
2025-08-10 12:14:34 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,40 @@
+fx_version 'cerulean'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+game 'rdr3'
+
+description 'rex-guarma'
+version '2.0.2'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua',
+}
+
+client_scripts {
+    'client/client.lua',
+    'client/npcs.lua',
+}
+
+server_scripts {
+    'server/server.lua',
+}
+
+dependencies {
+    'ox_lib',
+    'rsg-core',
+}
+
+files {
+  'locales/*.json'
+}
+
+-- makes files open-source
+escrow_ignore {
+    'client',
+    'locales',
+    'server',
+    'config.lua',
+    'README.md',
+}
+
+lua54 'yes'
